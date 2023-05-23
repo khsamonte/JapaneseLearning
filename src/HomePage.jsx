aef4da2e65d5557c721f70f5b729fc50da3ac7ac
khsamonte <khsamonte@gmail.com>
2023-05-23 15:34:16 +0800

    Testing cloned repo

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,28 +5,28 @@ import { Link } from "react-router-dom";
 localStorage.setItem("isAuthenticated", false);
 
 function Home() {
-	return (
-		<div className="home">
-			<h1>Personal Japanese Learning App</h1>
-			<div className="home__buttons">
-				<Link to="/vocabulary" className="home__button">
-					Vocabulary (Goi)
-				</Link>
-				<button className="home__button" disabled>
-					Sentence Pattern (Bunkei)
-				</button>
-				<button className="home__button" disabled>
-					Sample Sentence (Reibun)
-				</button>
-				<button className="home__button" disabled>
-					Exercises (Renshuu)
-				</button>
-				<button className="home__button" disabled>
-					Kanji
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="home">
+      <h1>Personal Japanese Learning App</h1>
+      <div className="home__buttons">
+        <Link to="/vocabulary" className="home__button">
+          Vocabulary (Goi)
+        </Link>
+        <button className="home__button" disabled>
+          Sentence Pattern (Bunkei)
+        </button>
+        <button className="home__button" disabled>
+          Sample Sentence (Reibun)
+        </button>
+        <button className="home__button" disabled>
+          Exercises (Renshuu)
+        </button>
+        <button className="home__button" disabled>
+          Kanji
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
