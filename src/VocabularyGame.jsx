6971fad7f553c6387921470001342a5c18c9283a
Ken Samonte <khsamonte@gmail.com>
2023-09-06 08:52:15 +0800

    Added Kanji 1-6

--- src/VocabularyGame.jsx
+++ src/VocabularyGame.jsx
@@ -5,128 +5,128 @@ import { shuffle } from "lodash";
 localStorage.setItem("isAuthenticated", true);
 
 function VocabularyGame({
-  vocabularies,
-  selectedCategories,
-  selectedQuestionCharacter,
-  selectedAnswerCharacter,
-  setSelectedAnswerCharacter,
+	vocabularies,
+	selectedCategories,
+	selectedQuestionCharacter,
+	selectedAnswerCharacter,
+	setSelectedAnswerCharacter,
 }) {
-  const [score, setScore] = useState(0);
-  const [total, setTotal] = useState(0);
-  const [result, setResult] = useState("");
-  const [wordList, setWordList] = useState([]);
-  const [currentWord, setCurrentWord] = useState({});
-  const [userAnswer, setUserAnswer] = useState("");
-  const [correctAnswer, setCorrectAnswer] = useState("");
-  const [gameIsDone, setGameIsDone] = useState(false);
+	const [score, setScore] = useState(0);
+	const [total, setTotal] = useState(0);
+	const [result, setResult] = useState("");
+	const [wordList, setWordList] = useState([]);
+	const [currentWord, setCurrentWord] = useState({});
+	const [userAnswer, setUserAnswer] = useState("");
+	const [correctAnswer, setCorrectAnswer] = useState("");
+	const [gameIsDone, setGameIsDone] = useState(false);
 
-  // Every time the categories are changed, the list is shuffled
-  useEffect(() => {
-    const list = shuffle(
-      selectedCategories.flatMap((category) => vocabularies[category])
-    );
-    setWordList(list);
-    setCurrentWord(list[0]);
-  }, [selectedCategories, vocabularies]);
+	// Every time the categories are changed, the list is shuffled
+	useEffect(() => {
+		const list = shuffle(
+			selectedCategories.flatMap((category) => vocabularies[category])
+		);
+		setWordList(list);
+		setCurrentWord(list[0]);
+	}, [selectedCategories, vocabularies]);
 
-  // When the user answers
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setTotal(total + 1);
+	// When the user answers
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setTotal(total + 1);
 
-    const userInput = userAnswer.toLowerCase();
-    let answer = currentWord[selectedAnswerCharacter];
+		const userInput = userAnswer.toLowerCase();
+		let answer = currentWord[selectedAnswerCharacter];
 
-    // If your answer is on an object property that is an array
-    if (Array.isArray(answer)) {
-      if (answer.includes(userInput)) {
-        setScore(score + 1);
-        setResult("correct");
-        setCorrectAnswer("");
-      } else {
-        setResult("wrong");
+		// If your answer is on an object property that is an array
+		if (Array.isArray(answer)) {
+			if (answer.includes(userInput)) {
+				setScore(score + 1);
+				setResult("correct");
+				setCorrectAnswer("");
+			} else {
+				setResult("wrong");
 
-        let substr = "";
-        answer.forEach((ans, idx) => {
-          if (idx === answer.length - 1) {
-            substr += ans;
-          } else {
-            substr += ans + " or ";
-          }
-          return substr;
-        });
-        setCorrectAnswer(substr);
-      }
-    } else if (userInput === answer.toLowerCase()) {
-      setScore(score + 1);
-      setResult("correct");
-      setCorrectAnswer("");
-    } else {
-      setCorrectAnswer(answer.toLowerCase());
-      setResult("wrong");
-    }
-    setUserAnswer("");
+				let substr = "";
+				answer.forEach((ans, idx) => {
+					if (idx === answer.length - 1) {
+						substr += ans;
+					} else {
+						substr += ans + " or ";
+					}
+					return substr;
+				});
+				setCorrectAnswer(substr);
+			}
+		} else if (userInput === answer.toLowerCase()) {
+			setScore(score + 1);
+			setResult("correct");
+			setCorrectAnswer("");
+		} else {
+			setCorrectAnswer(answer.toLowerCase());
+			setResult("wrong");
+		}
+		setUserAnswer("");
 
-    // If there are items remaining
-    if (wordList.length > 1) {
-      setWordList(wordList.slice(1));
-      setCurrentWord(wordList[1]);
-    } else {
-      setGameIsDone(true);
-      // const newList = shuffle(
-      // 	selectedCategories.flatMap((category) => vocabularies[category])
-      // );
-      // setWordList(newList);
-      // setCurrentWord(newList[0]);
-    }
-  };
+		// If there are items remaining
+		if (wordList.length > 1) {
+			setWordList(wordList.slice(1));
+			setCurrentWord(wordList[1]);
+		} else {
+			setGameIsDone(true);
+			// const newList = shuffle(
+			// 	selectedCategories.flatMap((category) => vocabularies[category])
+			// );
+			// setWordList(newList);
+			// setCurrentWord(newList[0]);
+		}
+	};
 
-  // Renders the word to display in the flash card
-  const renderWordDisplayed = () => {
-    /**
-     * Sometimes, the entry for a specific character is an array, so in that
-     * case, the first item inside that array will be chosen.
-     */
-    if (Array.isArray(currentWord[selectedQuestionCharacter.toLowerCase()])) {
-      return currentWord[selectedQuestionCharacter.toLowerCase()][0];
-    }
+	// Renders the word to display in the flash card
+	const renderWordDisplayed = () => {
+		/**
+		 * Sometimes, the entry for a specific character is an array, so in that
+		 * case, the first item inside that array will be chosen.
+		 */
+		if (Array.isArray(currentWord[selectedQuestionCharacter.toLowerCase()])) {
+			return currentWord[selectedQuestionCharacter.toLowerCase()][0];
+		}
 
-    // If the object property value is not an array, render normally
-    return currentWord[selectedQuestionCharacter.toLowerCase()];
-  };
+		// If the object property value is not an array, render normally
+		return currentWord[selectedQuestionCharacter.toLowerCase()];
+	};
 
-  return (
-    <div className="vocabulary-game-page">
-      {!gameIsDone ? (
-        <div className="game__word-display">{renderWordDisplayed()}</div>
-      ) : null}
-      <div className={`game__status ${gameIsDone ? "active" : ""}`}>
-        This set is done. Your score is:
-      </div>
-      <div className={`game__score ${result}`}>
-        Score: {score}/{total}
-      </div>
-      {correctAnswer !== "" && (
-        <div className={`game__correct_answer ${result}`}>
-          Correct Answer: {correctAnswer}
-        </div>
-      )}
-      <form className="game__input-form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={userAnswer}
-          onChange={(e) => setUserAnswer(e.target.value)}
-          required
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <div className="finish-button-container">
-        <Link to="/vocabulary" className="game__finish-button">
-          Done Practicing
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className="vocabulary-game-page">
+			{!gameIsDone ? (
+				<div className="game__word-display">{renderWordDisplayed()}</div>
+			) : null}
+			<div className={`game__status ${gameIsDone ? "active" : ""}`}>
+				This set is done. Your score is:
+			</div>
+			<div className={`game__score ${result}`}>
+				Score: {score}/{total}
+			</div>
+			{correctAnswer !== "" && (
+				<div className={`game__correct_answer ${result}`}>
+					Correct Answer: {correctAnswer}
+				</div>
+			)}
+			<form className="game__input-form" onSubmit={handleSubmit}>
+				<input
+					type="text"
+					value={userAnswer}
+					onChange={(e) => setUserAnswer(e.target.value)}
+					required
+				/>
+				<button type="submit">Submit</button>
+			</form>
+			<div className="finish-button-container">
+				<Link to="/vocabulary" className="game__finish-button">
+					Done Practicing
+				</Link>
+			</div>
+		</div>
+	);
 }
 
 export default VocabularyGame;
---
+++ src/data/vocab-kanji.js
@@ -0,0 +1,455 @@
+const kanji1 = [
+	{
+		kanji: "",
+		kana: "",
+	},
+];
+
+const kanji2 = [
+	{
+		kanji: "百",
+		kana: "ひゃく",
+	},
+	{
+		kanji: "千",
+		kana: "せん",
+	},
+	{
+		kanji: "万",
+		kana: "まん",
+	},
+	{
+		kanji: "日",
+		kana: "ひ",
+	},
+	{
+		kanji: "休日",
+		kana: "きゅうじつ",
+	},
+	{
+		kanji: "月",
+		kana: "つき",
+	},
+	{
+		kanji: "今月",
+		kana: "こんげつ",
+	},
+	{
+		kanji: "火",
+		kana: "ひ",
+	},
+	{
+		kanji: "火山",
+		kana: "かざん",
+	},
+	{
+		kanji: "火事",
+		kana: "かじ",
+	},
+	{
+		kanji: "水",
+		kana: "みず",
+	},
+	{
+		kanji: "木",
+		kana: "き",
+	},
+	{
+		kanji: "お金",
+		kana: "おかね",
+	},
+	{
+		kanji: "金",
+		kana: "きん",
+	},
+	{
+		kanji: "土",
+		kana: "つち",
+	},
+];
+
+const kanji3 = [
+	{
+		kanji: "円",
+		kana: "えん",
+	},
+	{
+		kanji: "何",
+		kana: "なに",
+	},
+	{
+		kanji: "何か",
+		kana: "なにか",
+	},
+	{
+		kanji: "本",
+		kana: "ほん",
+	},
+	{
+		kanji: "人",
+		kana: "ひと",
+	},
+	{
+		kanji: "男",
+		kana: "おとこ",
+	},
+	{
+		kanji: "男の子",
+		kana: "おとこのこ",
+	},
+	{
+		kanji: "男性",
+		kana: "だんせい",
+	},
+	{
+		kanji: "女",
+		kana: "おんな",
+	},
+	{
+		kanji: "女の子",
+		kana: "おんなのこ",
+	},
+	{
+		kanji: "女性",
+		kana: "じょせい",
+	},
+	{
+		kanji: "子ども",
+		kana: "こども",
+	},
+	{
+		kanji: "男子",
+		kana: "だんし",
+	},
+	{
+		kanji: "女子",
+		kana: "じょし",
+	},
+	{
+		kanji: "父",
+		kana: "ちち",
+	},
+	{
+		kanji: "お父さん",
+		kana: "おとうさん",
+	},
+	{
+		kanji: "母",
+		kana: "はは",
+	},
+	{
+		kanji: "お母さん",
+		kana: "おかあさん",
+	},
+	{
+		kanji: "友だち",
+		kana: "ともだち",
+	},
+	{
+		kanji: "友人",
+		kana: "ゆうじん",
+	},
+];
+
+const kanji4 = [
+	{
+		kanji: "半分",
+		kana: "はんぶん",
+	},
+	{
+		kanji: "時",
+		kana: "とき",
+	},
+	{
+		kanji: "時計",
+		kana: "とけい",
+	},
+	{
+		kanji: "時間",
+		kana: "じかん",
+	},
+	{
+		kanji: "今週",
+		kana: "こんしゅう",
+	},
+	{
+		kanji: "一週間",
+		kana: "いっしゅうかん",
+	},
+	{
+		kanji: "今年",
+		kana: "ことし",
+	},
+	{
+		kanji: "間",
+		kana: "あいだ",
+	},
+	{
+		kanji: "今",
+		kana: "いま",
+	},
+	{
+		kanji: "今日",
+		kana: "きょう",
+	},
+	{
+		kanji: "先",
+		kana: "さき",
+	},
+	{
+		kanji: "先生",
+		kana: "せんせい",
+	},
+	{
+		kanji: "先週",
+		kana: "せんしゅう",
+	},
+	{
+		kanji: "先月",
+		kana: "せんげつ",
+	},
+	{
+		kanji: "来る",
+		kana: "来ます",
+	},
+	{
+		kanji: "来週",
+		kana: "らいしゅう",
+	},
+	{
+		kanji: "来月",
+		kana: "らいげつ",
+	},
+	{
+		kanji: "来年",
+		kana: "らいねん",
+	},
+	{
+		kanji: "毎年",
+		kana: "まいとし",
+	},
+];
+
+const kanji5 = [
+	{
+		kanji: "午前",
+		kana: "ごぜん",
+	},
+	{
+		kanji: "前",
+		kana: "まえ",
+	},
+	{
+		kanji: "名前",
+		kana: "なまえ",
+	},
+	{
+		kanji: "午前中",
+		kana: "ごぜんちゅう",
+	},
+	{
+		kanji: "後",
+		kana: "あと",
+	},
+	{
+		kanji: "後ろ",
+		kana: "うしろ",
+	},
+	{
+		kanji: "午後",
+		kana: "ごご",
+	},
+	{
+		kanji: "朝",
+		kana: "あさ",
+	},
+	{
+		kanji: "今朝",
+		kana: "けさ",
+	},
+	{
+		kanji: "毎朝",
+		kana: "まいあさ",
+	},
+	{
+		kanji: "一晩中",
+		kana: "ひとばんじゅう",
+	},
+	{
+		kanji: "毎晩",
+		kana: "まいばん",
+	},
+	{
+		kanji: "今晩",
+		kana: "こんばん",
+	},
+	{
+		kanji: "夏",
+		kana: "なつ",
+	},
+	{
+		kanji: "春",
+		kana: "はる",
+	},
+	{
+		kanji: "冬",
+		kana: "ふゆ",
+	},
+	{
+		kanji: "秋",
+		kana: "あき",
+	},
+	{
+		kanji: "正しい",
+		kana: "ただしい",
+	},
+	{
+		kanji: "正月",
+		kana: "しょうがつ",
+	},
+	{
+		kanji: "正月休み",
+		kana: "しょうがつやすみ",
+	},
+];
+
+const kanji6 = [
+	{
+		kanji: "上",
+		kana: "うえ",
+	},
+	{
+		kanji: "上着",
+		kana: "うわぎ",
+	},
+	{
+		kanji: "上げる",
+		kana: "あげる",
+	},
+	{
+		kanji: "上手",
+		kana: "じょうず",
+	},
+	{
+		kanji: "下",
+		kana: "した",
+	},
+	{
+		kanji: "下げる",
+		kana: "さげる",
+	},
+	{
+		kanji: "下手な",
+		kana: "へたな",
+	},
+	{
+		kanji: "下着",
+		kana: "したぎ",
+	},
+	{
+		kanji: "左",
+		kana: "ひだり",
+	},
+	{
+		kanji: "左側",
+		kana: "ひたりがわ",
+	},
+	{
+		kanji: "右",
+		kana: "みぎ",
+	},
+	{
+		kanji: "右側",
+		kana: "みぎがわ",
+	},
+	{
+		kanji: "左右",
+		kana: "さゆう",
+	},
+	{
+		kanji: "中",
+		kana: "なか",
+	},
+	{
+		kanji: "一日中",
+		kana: "いちにちじゅう",
+	},
+	{
+		kanji: "一晩中",
+		kana: "ひとばんじゅう",
+	},
+	{
+		kanji: "一年中",
+		kana: "いちねんじゅう",
+	},
+	{
+		kanji: "午前中",
+		kana: "ごぜんちゅう",
+	},
+	{
+		kanji: "中国",
+		kana: "ちゅうごく",
+	},
+	{
+		kanji: "外",
+		kana: "そと",
+	},
+	{
+		kanji: "外国",
+		kana: "がいこく",
+	},
+	{
+		kanji: "外国人",
+		kana: "がいこくじん",
+	},
+	{
+		kanji: "外国語",
+		kana: "がいこくご",
+	},
+	{
+		kanji: "外出",
+		kana: "がいしゅつ",
+	},
+	{
+		kanji: "海外",
+		kana: "かいがい",
+	},
+	{
+		kanji: "北",
+		kana: "きた",
+	},
+	{
+		kanji: "北口",
+		kana: "きたぐち",
+	},
+	{
+		kanji: "南",
+		kana: "みなみ",
+	},
+	{
+		kanji: "南口",
+		kana: "みなみぐち",
+	},
+	{
+		kanji: "西",
+		kana: "にし",
+	},
+	{
+		kanji: "西口",
+		kana: "にしぐち",
+	},
+	{
+		kanji: "東",
+		kana: "ひがし",
+	},
+	{
+		kanji: "東口",
+		kana: "ひがしぐち",
+	},
+	{
+		kanji: "東京",
+		kana: "とうきょう",
+	},
+];
+
+export { kanji1, kanji2, kanji3, kanji4, kanji5, kanji6 };
